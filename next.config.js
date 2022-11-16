6e08220740b6446a7e5ff5a913707603a0aa8657
宰川大介 <daisuke@saikawadaisukenoMacBook-Pro.local>
2022-11-17 00:13:23 +0900

    Add pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
+
+module.exports = withPWA({
   reactStrictMode: true,
   staticPageGenerationTimeout: 2000,
-};
-
-module.exports = nextConfig;
+});
