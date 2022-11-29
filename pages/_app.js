2416de822a356ec7f3d3b357a516f9cb29ad9d18
宰川大介 <daisuke@saikawadaisukenoMacBook-Pro.local>
2022-11-29 18:06:33 +0900

    Add title

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Head>
+        <title>saisuke.div</title>
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
